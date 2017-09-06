2b6d3aba820b82d4e6ba4d903c285cf3211b82ce
Shad Downey <shad@1stdibs.com>
2017-09-06 17:19:46 -0400

    v0.1

--- InterviewQuestion-2/ViewController.swift
+++ InterviewQuestion-2/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
+/**
+ - In the main content view, construct a histogram (with columns for 1 through 12)
+
+ - Whenever "ROLL" is tapped, randomly select values for the left and right "die" between 1 and 6, inclusive.
+
+ - After values are selected, update the UI, and add an entry to the histogram for the two values' sum
+ */
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var leftDie: UIButton!
+    @IBOutlet weak var rightDie: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
